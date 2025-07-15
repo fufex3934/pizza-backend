0b59fa56005f3367a98a90968e43f940e8505342
fufex3934 <fufawakgari174@gmail.com>
2025-07-16 00:05:03 +0300

    feat: generate restaurant module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CustomConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { RestaurantsModule } from './modules/restaurants/restaurants.module';
 
 @Module({
-  imports: [CustomConfigModule, DatabaseModule, AuthModule, UsersModule],
+  imports: [CustomConfigModule, DatabaseModule, AuthModule, UsersModule, RestaurantsModule],
   controllers: [AppController],
   providers: [AppService],
 })

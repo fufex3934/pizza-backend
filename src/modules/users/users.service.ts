0f5a9e5fc454d575365938165ce1b0a9cfff48b4
fufex3934 <fufawakgari174@gmail.com>
2025-07-16 00:03:15 +0300

    feat: implement user service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { ErrorCode } from 'src/common/constants/error.constants';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async findById(id: string): Promise<User> {
+    const user = await this.userModel.findById(id).exec();
+    if (!user) {
+      throw new NotFoundException('User not foun', ErrorCode.USER_NOT_FOUND);
+    }
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userModel
+      .findOne({
+        email,
+      })
+      .exec();
+  }
+
+  async updateUser(userId: string, updateData: Partial<User>): Promise<User> {
+    const user = await this.userModel.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+    if (!user) {
+      throw new NotFoundException('User not found', ErrorCode.USER_NOT_FOUND);
+    }
+    return user;
+  }
+
+  async removeUser(userId: string): Promise<User | null> {
+    return this.userModel.findByIdAndDelete(userId);
+  }
+}

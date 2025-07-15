d12d73585cf4fba4990eae679b5279428469c9c9
fufex3934 <fufawakgari174@gmail.com>
2025-07-15 22:26:50 +0300

    feat:define jwt strategy

---
+++ src/common/interfaces/token-payload.interface.ts
@@ -0,0 +1,6 @@
+export interface TokenPayload {
+  userId: string;
+  email: string;
+  type: string;
+  restaurantId: string;
+}
---
+++ src/common/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') as string,
+    });
+  }
+  validate(payload: TokenPayload): TokenPayload {
+    return {
+      userId: payload.userId,
+      email: payload.email,
+      type: payload.type,
+      restaurantId: payload.restaurantId,
+    };
+  }
+}

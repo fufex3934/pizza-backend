8ac3bf029c65f4699969679989d2569273a389d5
fufex3934 <fufawakgari174@gmail.com>
2025-07-20 19:40:44 +0300

    feat: implement Role service

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Role, RoleDocument } from './schemas/role.schema';
+import { Model } from 'mongoose';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { ErrorCode } from 'src/common/constants/error.constants';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(
+    @InjectModel(Role.name)
+    private roleModel: Model<RoleDocument>,
+  ) {}
+
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
+    const existingRole = await this.roleModel.findOne({
+      name: createRoleDto.name,
+      restaurantId: createRoleDto.permissions,
+    });
+
+    if (existingRole) {
+      throw new ConflictException(
+        'Role with this name already exists',
+        ErrorCode.CONFLICT,
+      );
+    }
+    const role = new this.roleModel(createRoleDto);
+    return role.save();
+  }
+
+  async findAllByRestaurant(restaurantId: string): Promise<Role[]> {
+    return this.roleModel.find({ restaurantId }).exec();
+  }
+
+  async findOne(id: string, restaurantId: string): Promise<Role> {
+    const role = await this.roleModel
+      .findOne({
+        _id: id,
+        restaurantId,
+      })
+      .exec();
+    if (!role) {
+      throw new NotFoundException(
+        'Role not found',
+        ErrorCode.RESOURCE_NOT_FOUND,
+      );
+    }
+    return role;
+  }
+
+  async getDefaultRoles(restaurantId: string): Promise<Role[]> {
+    return this.roleModel.find({ restaurantId, isDefault: true }).exec();
+  }
+}
